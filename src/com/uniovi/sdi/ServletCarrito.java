dc299838a262735ab5f8d9a89d435a273ac119ae
UO271288 <uo271288@uniovi.es>
2021-02-01 07:04:14 +0100

    SDI-2021-901-1.8-MVC, Modelo Vista Controlador.

--- src/com/uniovi/sdi/ServletCarrito.java
+++ src/com/uniovi/sdi/ServletCarrito.java
@@ -1,7 +1,6 @@
 package com.uniovi.sdi;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
@@ -33,27 +32,20 @@ public class ServletCarrito extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		synchronized (session) {
-			@SuppressWarnings("unchecked")
-			HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito");
-			// No hay carrito, creamos uno y lo insertamos en sesión
-			if (carrito == null) {
-				carrito = new HashMap<String, Integer>();
-				request.getSession().setAttribute("carrito", carrito);
-			}
-			String producto = request.getParameter("producto");
-			if (producto != null) {
-				insertarEnCarrito(carrito, producto);
-			}
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("text/html");
-			PrintWriter out = response.getWriter();
-			out.println("<HTML>");
-			out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
-			out.println("<BODY>");
-			out.println(carritoEnHTML(carrito) + "<br>");
-			out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+		@SuppressWarnings("unchecked")
+		HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito");
+		// No hay carrito, creamos uno y lo insertamos en sesión
+		if (carrito == null) {
+			carrito = new HashMap<String, Integer>();
+			request.getSession().setAttribute("carrito", carrito);
 		}
+		String producto = request.getParameter("producto");
+		if (producto != null) {
+			insertarEnCarrito(carrito, producto);
+		}
+		// Retornar la vista con parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 	}
 
 	/**
