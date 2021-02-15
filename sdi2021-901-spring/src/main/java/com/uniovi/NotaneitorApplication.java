c8e491f407c8a520a3245b438ce2aa22f6241afe
UO271288 <uo271288@uniovi.es>
2021-02-15 01:39:38 +0100

    SDI-2021-901-2.2-Trabajando con Controladores.

--- sdi2021-901-spring/src/main/java/com/uniovi/NotaneitorApplication.java
+++ sdi2021-901-spring/src/main/java/com/uniovi/NotaneitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sdi2021901SpringApplication {
+public class NotaneitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sdi2021901SpringApplication.class, args);
+		SpringApplication.run(NotaneitorApplication.class, args);
 	}
 
 }
--- sdi2021-901-spring/src/test/java/com/uniovi/NotaneitorApplicationTests.java
+++ sdi2021-901-spring/src/test/java/com/uniovi/NotaneitorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sdi2021901SpringApplicationTests {
+class NotaneitorApplicationTests {
 
 	@Test
 	void contextLoads() {
