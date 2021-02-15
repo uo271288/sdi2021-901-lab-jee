86d4f9c17645e6a5bfe20d3b9190a7cd1b123298
UO271288 <uo271288@uniovi.es>
2021-02-15 02:43:34 +0100

    SDI-2021-901-2.4-Modelo, acceso a datos simple.

--- sdi2021-901-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi2021-901-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -1,6 +1,12 @@
 package com.uniovi.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Mark {
+	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String description;
 	private Double score;
