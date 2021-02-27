61c68ce2517db21c6f57b117a58135ce0513d26f
UO271288 <uo271288@uniovi.es>
2021-02-27 17:40:28 +0100

    SDI-2021-901-4.5-Búsqueda.

--- sdi2021-901-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
+++ sdi2021-901-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
@@ -11,6 +11,12 @@ import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
+	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1))")
+	List<Mark> searchByDescriptionAndName(String searchtext);
+
+	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1) AND r.user = ?2)")
+	List<Mark> searchByDescriptionNameAndUser(String searchtext, User user);
+
 	@Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC ")
 	List<Mark> findAllByUser(User user);
 
