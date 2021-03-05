55ec7e2fde826416f7ce98838c6b96d2427c388f
UO271288 <uo271288@uniovi.es>
2021-03-05 16:04:47 +0100

    SDI-2021-901-5.4-Casos Uso VistaHome.

--- sdi2021-901-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-901-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,7 +1,5 @@
 package com.uniovi.tests;
 
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,44 +10,74 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
-	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
-	// automáticas)):
-	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-	static String Geckdriver024 = "C:\\Users\\MediaSiero\\Desktop\\Cosas\\Universidad\\3º\\2º Semestre\\SDI\\Practica\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
-	static String URL = "http://localhost:8090";
+    // En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+    // automáticas)):
+    static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    static String Geckdriver024 = "C:\\Users\\MediaSiero\\Desktop\\Cosas\\Universidad\\3º\\2º Semestre\\SDI\\Practica\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
+    static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+    static String URL = "http://localhost:8090";
 
-	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
-		System.setProperty("webdriver.firefox.bin", PathFirefox);
-		System.setProperty("webdriver.gecko.driver", Geckdriver);
-		WebDriver driver = new FirefoxDriver();
-		return driver;
-	}
+    public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+	System.setProperty("webdriver.firefox.bin", PathFirefox);
+	System.setProperty("webdriver.gecko.driver", Geckdriver);
+	WebDriver driver = new FirefoxDriver();
+	return driver;
+    }
 
-	// Antes de cada prueba se navega al URL home de la aplicación
-	@Before
-	public void setUp() {
-		driver.navigate().to(URL);
-	} // Después de cada prueba se borran las cookies del navegador
+    // Antes de cada prueba se navega al URL home de la aplicación
+    @Before
+    public void setUp() {
+	driver.navigate().to(URL);
+    }
 
-	@After
-	public void tearDown() {
-		driver.manage().deleteAllCookies();
-	} // Antes de la primera prueba
+    // Después de cada prueba se borran las cookies del navegador
+    @After
+    public void tearDown() {
+	driver.manage().deleteAllCookies();
+    }
 
-	@BeforeClass
-	static public void begin() {
-	} // Al finalizar la última prueba
+    // Antes de la primera prueba
+    @BeforeClass
+    static public void begin() {
+    }
 
-	@AfterClass
-	static public void end() { // Cerramos el navegador al finalizar las pruebas
-		driver.quit();
-	}
+    // Al finalizar la última prueba
+    @AfterClass
+    static public void end() {
+	// Cerramos el navegador al finalizar las pruebas
+	driver.quit();
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    // PR01. Acceder a la página principal /
+    @Test
+    public void PR01() {
+	PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+    }
+
+    // PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+    @Test
+    public void PR02() {
+	PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
+
+    // PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+    // home
+    @Test
+    public void PR03() {
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+
+    // PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+    // Español
+    @Test
+    public void PR04() {
+	PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+		PO_Properties.getENGLISH());
+	// SeleniumUtils.esperarSegundos(driver, 2);
+    }
 }
